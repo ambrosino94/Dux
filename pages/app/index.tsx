45ed573644dded22fe56af5e5c8466ed39fe03a3
lbozzo <lucas.bozzo@icloud.com>
2021-10-10 18:18:31 +0100

    Base navigation

---
+++ pages/app/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const AppHome: NextPage = () => {
+  return (
+    <>
+      <h1>App content</h1>
+    </>
+  );
+};
+
+export default AppHome;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import NextLink from "next/link";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <div style={{ margin: 50 }}>
+      <h1>Dux</h1>
+
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <NextLink href="/app">App</NextLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
